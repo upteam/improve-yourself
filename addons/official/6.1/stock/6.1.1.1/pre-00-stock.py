11873b05ae106727e26721beb0fe2010c3007236
Christophe CHAUVET <christophe.chauvet@gmail.com>
2013-02-16 18:43:00 +0100

    [ADD] Add first commit of the solution

---
+++ addons/official/6.1/stock/6.1.1.1/pre-00-stock.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    migration module for OpenERP, Script to migrate version
+#    Copyright (C) 2012 UPTEAM (<http://github.com/upteam/improve-yourself>) Christophe CHAUVET
+#
+#    This file is a part of migration
+#
+#    migration is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    migration is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+logger = logging.getLogger('migration.stock')
+
+
+def migrate(cr, v):
+    """
+    Premier script en point d'entrée.
+    """
+
+    logger.info('Alter stock structure')
+    # TODO Check this delete
+    #reports cannot have both report_xsl and report_rml
+    #update ir_act_report_xml set report_xsl = null where ...
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
