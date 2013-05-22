c62bdc49c8ba3880efa0d3a3e9aaa8ec0f85ff86
UpTeam <upteam@gmail.com>
2013-05-22 15:23:31 +0200

    [ADD] Add first file to migrate from 5.0 to 7.0

---
+++ addons/official/5.0-7.0/base/7.0.1.3/pre-00-initial.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    migration module for OpenERP, Script to migrate version
+#    Copyright (C) 2012-2013 UPTEAM (<http://github.com/upteam/improve-yourself>) Christophe CHAUVET
+#
+#    This file is a part of migration
+#
+#    migration is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    migration is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import sys
+import logging
+logger = logging.getLogger('migration.base')
+
+def table_exists(cr, tablename):
+    """
+    Return True if table exists
+    """
+    cr.execute("""SELECT * FROM pg_tables WHERE tablename=%s;""", (tablename,))
+    if cr.fetchone():
+        return True
+    return False
+
+def migrate(cr, v):
+    """
+    Premier script en point d'entrée.
+    """
+    pass
+
+
